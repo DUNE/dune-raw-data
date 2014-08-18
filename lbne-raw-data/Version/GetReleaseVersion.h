b248874f500b64709009057607a4f8ea7b111dc4
LBNE DAQ <lbnedaq@lbne35t-gateway01.fnal.gov>
2014-08-18 17:56:04 -0500

    Include code designed to provide the version and build time of the package

---
+++ lbne-raw-data/Version/GetReleaseVersion.h
@@ -0,0 +1,15 @@
+#ifndef lbne_raw_data_Version_GetReleaseVersion_h
+#define lbne_raw_data_Version_GetReleaseVersion_h
+
+#include <string>
+
+namespace lbnerawdata {
+  std::string const & getReleaseVersion();
+  std::string const & getBuildDateTime();
+}
+
+#endif /* lbne_raw_data_Version_GetReleaseVersion_h */
+
+// Local Variables:
+// mode: c++
+// End:

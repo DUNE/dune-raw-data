debdee062de2c50126c9c011f50282c2888355c0
Thomas Dealtry <thomas.dealtry@physics.ox.ac.uk>
2015-02-02 10:55:09 -0600

    Add first versions of PennMilliSliceFragment and PennMilliSliceWriter classes

---
+++ lbne-raw-data/Overlays/PennMilliSliceWriter.hh
@@ -0,0 +1,53 @@
+#ifndef lbne_artdaq_Overlays_PennMillisliceWriter_hh
+#define lbne_artdaq_Overlays_PennMilliSliceWriter_hh
+
+#include "lbne-raw-data/Overlays/PennMilliSlice.hh"
+//#include "lbne-raw-data/Overlays/PennMicroSliceWriter.hh" TODO
+
+namespace lbne {
+  class PennMilliSliceWriter;
+}
+
+class lbne::PennMilliSliceWriter : public lbne::PennMilliSlice {
+
+public:
+
+  // This constructor creates an empty PennMilliSlice which can be filled
+  // with the appropriate data
+  PennMilliSliceWriter(uint8_t* address, uint32_t max_size_bytes);
+
+  // Reserves the next MicroSlice in memory within this PennMilliSlice.
+  // The MicroSliceWriter that is returned is initialized to an empty
+  // state and is ready to be populated with data.  This method returns
+  // true if the MicroSlice was successfully added, false if not (for example,
+  // if the additional MicroSlice would overflow the maximum size of the
+  // PennMilliSlice).
+  // std::shared_ptr<MicroSliceWriter> reserveMicroSlice(uint32_t ms_max_bytes); TODO ??
+
+  // Finalizes the PennMilliSlice.  This takes care of updating the
+  // PennMilliSlice so that all internal pointers and data are consistent
+  // with the MicroSlices that have been added.  No more MicroSlices
+  // can be added once the PennMilliSlice has been finalized.
+  // This method returns the number of bytes that were reclaimed
+  // when the maximum size for this PennMilliSlice was reduced to match
+  // its actual current size.
+  int32_t finalize(uint32_t data_size_bytes, uint32_t microslice_count);
+
+protected:
+
+  // finalizes the MicroSlice that was most recently added and
+  // updates our size to reflect any memory that was reclaimed
+  // by finalizing the MicroSlice
+  // void finalizeLatestMicroSlice_(); TODO
+
+  // returns a pointer to the header
+  Header* header_();
+
+  // returns a pointer to the requested MicroSlice
+  // uint8_t* data_(int index); TODO
+
+  uint32_t max_size_bytes_;
+  // std::shared_ptr<MicroSliceWriter> latest_microslice_ptr_; TODO
+};
+
+#endif /* lbne_artdaq_Overlays_PennMilliSliceWriter_hh */

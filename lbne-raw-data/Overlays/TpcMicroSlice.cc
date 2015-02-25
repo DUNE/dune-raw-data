778c1e6ed779b16f05fde3cc6065fbec4ec31c45
mgraham <mgraham@slac.stanford.edu>
2015-02-25 09:00:17 -0800

    minor changes to TPC overlays

--- lbne-raw-data/Overlays/TpcMicroSlice.cc
+++ lbne-raw-data/Overlays/TpcMicroSlice.cc
@@ -9,23 +9,42 @@ lbne::TpcMicroSlice::TpcMicroSlice(uint8_t* address) : buffer_(address)
 // Returns the size of the TpcMicroSlice
 lbne::TpcMicroSlice::Header::microslice_size_t lbne::TpcMicroSlice::size() const
 {
-
-  // Temporary modification for interim uslice format - size is in word 0 in bytes
-	return (header_()->raw_header_data[0]);
+  // Temporary modification for interim uslice format - size is in word 0 in bytes  
+  return (header_()->raw_header_data[0]);
 }
 
 // Returns the sequence ID of the TpcMicroSlice
 lbne::TpcMicroSlice::Header::sequence_id_t lbne::TpcMicroSlice::sequence_id() const
 {
-  // Temporary modification for interim uslice format - size is in word 0 in bytes
   // Sequence ID is word 1 of header
   return reinterpret_cast<lbne::TpcMicroSlice::Header::sequence_id_t>(header_()->raw_header_data[1]);
 }
 
+// Returns the type ID of the TpcMicroSlice
+lbne::TpcMicroSlice::Header::type_id_t lbne::TpcMicroSlice::type_id() const
+{
+  // type ID is word 2 of header
+  return reinterpret_cast<lbne::TpcMicroSlice::Header::type_id_t>(header_()->raw_header_data[2]);
+}
+
+// Returns the software message of the TpcMicroSlice
+lbne::TpcMicroSlice::Header::softmsg_t lbne::TpcMicroSlice::software_message() const
+{
+  // software message is words 3 & 4  of header
+  return reinterpret_cast<lbne::TpcMicroSlice::Header::softmsg_t>(((uint64_t)header_()->raw_header_data[3])<<32|header_()->raw_header_data[4]);
+}
+
+// Returns the firmware message of the TpcMicroSlice
+lbne::TpcMicroSlice::Header::firmmsg_t lbne::TpcMicroSlice::firmware_message() const
+{
+  // firmware message is words 5 & 6  of header
+  return reinterpret_cast<lbne::TpcMicroSlice::Header::firmmsg_t>(((uint64_t)header_()->raw_header_data[5])<<32|header_()->raw_header_data[6]);
+}
+
+
 // Returns the number of nanoslices in the microslice
 lbne::TpcMicroSlice::Header::nanoslice_count_t lbne::TpcMicroSlice::nanoSliceCount() const
 {
-
 	// Temporary hack - retrieve nanoslice size from first nanoslice, then infer
 	// nanoslice count based on size of microslice. This is potentially brittle, and
 	// should be encoded in the microslice header instead

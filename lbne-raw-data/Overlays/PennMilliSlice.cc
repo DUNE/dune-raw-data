90eebbf2dcd766cf1ae011394810a2620de39176
Nuno Barros <nfbarros@hep.upenn.edu>
2016-02-18 23:18:25 -0600

    NFB: Commented out PennMillislice checksum generation.

--- lbne-raw-data/Overlays/PennMilliSlice.cc
+++ lbne-raw-data/Overlays/PennMilliSlice.cc
@@ -388,11 +388,12 @@ uint8_t* lbne::PennMilliSlice::payload(uint32_t index,
   return nullptr;
 }
 
+#ifdef ENABLE_PENNMILLISLICE_CHECKSUM
 
 lbne::PennMilliSlice::checksum_t lbne::PennMilliSlice::calculateChecksum() const
 {
   try {
-    //NFB: This is not the right algorithm to calculate the checksum, I think
+    //NFB: Is the checksum actually used anywhere? It certainly complicates the calculations.
     boost::crc_32_type checksum;
     checksum.process_bytes(buffer_, this->size());
     return checksum.checksum();
@@ -408,6 +409,7 @@ lbne::PennMilliSlice::checksum_t lbne::PennMilliSlice::checksum() const
 {
   return *(reinterpret_cast<lbne::PennMilliSlice::checksum_t*>(buffer_ + this->size() - sizeof(lbne::PennMilliSlice::checksum_t)));
 }
+#endif
 
 lbne::PennMilliSlice::Header const* lbne::PennMilliSlice::header_() const
 {

d5cad6e5c533b15b1e7d127814f86b6a6efa55cb
Jingbo Wang <jiowang@ucdavis.edu>
2018-08-02 14:33:39 +0200

    Added Fragment type for CTB fragment

--- dune-raw-data/Overlays/FragmentType.hh
+++ dune-raw-data/Overlays/FragmentType.hh
@@ -5,7 +5,7 @@
 namespace dune {
 
   static std::vector<std::string> const
-  names { "MISSED", "TPC", "PHOTON", "TRIGGER", "TIMING", "TOY1", "TOY2", "FELIX", "CRT", "UNKNOWN" };
+  names { "MISSED", "TPC", "PHOTON", "TRIGGER", "TIMING", "TOY1", "TOY2", "FELIX", "CRT", "CTB", "UNKNOWN" };
 
   namespace detail {
     enum FragmentType : artdaq::Fragment::type_t
@@ -18,6 +18,7 @@ namespace dune {
         TOY2,
         FELIX,
         CRT,
+	CTB,
         INVALID // Should always be last.
         };
 
